0e65912ab6044e3fdaa1421d4e7bdd9f4e9d720e
Mbanzoulou <o2mhova@gmail.com>
2025-08-30 01:47:02 +0100

    created the user model class

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,9 +4,34 @@ namespace app\Controllers;
 
 class AuthController
 {
+    private $userModel;
+
+    public function __construct($userModel)
+    {
+        $this->userModel = $userModel;
+    }
     public function login()
     {
         require_once VIEWS_PATH . '/login.php';
+    }
+
+    public function authenticate()
+    {
+        // Handle authentication logic
+        $name = $_POST['username'];
+        $password = $_POST['password'];
+        $user = $this->userModel->findByUsername($name);
+
+        if ($user && password_verify($password, $user['password'])) {
+            session_start();
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['role'] = $user['role'];
+            header('Location: /dashboard'); // Redirect to a protected page
+        } else {
+            echo "Invalid credentials. Please try again.";
+        }
+
     }
 
     public function logout()
---
+++ app/Models/user.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\Models;
+
+class User
+{
+    private $id;
+    private $username;
+    private $passwordHash;
+    private $role;
+    private $conn;
+
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+   
+    public function save($username, $password, $role)
+    {
+        $stmt = $this->conn->prepare("INSERT INTO users (username, password, role) VALUES (?, ?, ?)");
+        $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+        return $stmt->execute([$username, $passwordHash, $role]);
+    }
+
+    public function findByUsername($username)
+    {
+        $stmt = $this->conn->prepare("SELECT * FROM users WHERE username = ?");
+        $stmt->execute([$username]);
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+}
